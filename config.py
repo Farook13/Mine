f0741831e083da7f395f92c471dd252c265cc6c2
Farook13 <umarulfarook787@gmail.com>
2025-05-27 17:39:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     API_ID = int(os.environ.get("API_ID", 12618934))
     API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7955983025:AAECeT09wQ9ptCDnJ42YmKjy7rMSv9k4eiw")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7857321740:AAHSUfjwO3w6Uffmxm9vCUMl36FtXl5-r6w")
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://batman13:batman13@batman.sawvl.mongodb.net/?retryWrites=true&w=majority&appName=batman")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "TelegramBot")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100233236188))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002332361885))
     FORCE_SUB = os.environ.get("FORCE_SUB", None)  # Channel username or ID
     ADMINS = list(map(int, os.environ.get("ADMINS", "5032034594").split()))
 
