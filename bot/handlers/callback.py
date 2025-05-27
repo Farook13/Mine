bd0f03b085e91b8244ae9c942542ce2a647ce551
Farook13 <umarulfarook787@gmail.com>
2025-05-27 16:21:27 +0530

    Update callback.py

--- bot/handlers/callback.py
+++ bot/handlers/callback.py
@@ -6,3 +6,4 @@ async def help_cb(client, callback_query: CallbackQuery):
     await callback_query.message.edit_text(
         "**Help Menu**\n\nSend a movie or file name to search.\nUse /start to restart.",
         reply_markup=callback_query.message.reply_markup
+    )
