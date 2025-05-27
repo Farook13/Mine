a0a8a1c595fa5a6654bb92918259d6d5a10a200c
Farook13 <umarulfarook787@gmail.com>
2025-05-27 17:07:25 +0530

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -13,4 +13,4 @@ async def start_handler(client, message: Message):
         photo=Config.START_PIC or "https://telegra.ph/file/6c2d8c33f24c7fc83b3d9.jpg",
         caption=f"Hello {message.from_user.mention}, welcome to the fastest AutoFilter bot!",
         reply_markup=InlineKeyboardMarkup(buttons)
-    
+    )  # <-- This line was missing!
