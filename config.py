e2e275bef5c2fc098d50dfc04a536b9d2a516fb6
Farook13 <umarulfarook787@gmail.com>
2025-05-27 16:50:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
-    MONGO_URI = os.environ.get("MONGO_URI", "your_mongo_uri")
+    API_ID = int(os.environ.get("API_ID", 12618934))
+    API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7693803634:AAFIWfW8gfzMYv-G5-I9hAnge1mYFVkspio")
+    MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://batman13:batman13@batman.sawvl.mongodb.net/?retryWrites=true&w=majority&appName=batman")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "TelegramBot")
 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1001234567890))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100233236188))
     FORCE_SUB = os.environ.get("FORCE_SUB", None)  # Channel username or ID
-    ADMINS = list(map(int, os.environ.get("ADMINS", "123456789").split()))
+    ADMINS = list(map(int, os.environ.get("ADMINS", "5032034594").split()))
 
     WELCOME_MESSAGE = os.environ.get("WELCOME_MESSAGE", "Welcome {mention}! Use me to get your files instantly.")
     FILE_CAPTION_TEMPLATE = os.environ.get(
